1139a503eacaef8591467fe9945b70e4d3be3b7b
Jan Claasen <janclaasen@arpmanet-2.local>
2020-08-16 09:18:42 +1200

    added fetch label for the creation of the x axis labels on the chart

--- static/index.js
+++ static/index.js
@@ -1,14 +1,23 @@
 import modelData from "./data.js";
 const canvas = document.getElementById("myChart");
 const ctx = canvas.getContext("2d");
-console.log(modelData);
+const fetchLabel = () => {
+  let labelArray = [];
+  for (const month of modelData) {
+    if (month.state_abbrev == "ACT") {
+      labelArray.push(month.month);
+    }
+  }
+  return labelArray;
+};
+
 const myChart = new Chart(ctx, {
   type: "line",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: fetchLabel(),
     datasets: [
       {
-        label: "# of Votes",
+        label: modelData[0].state_abbrev,
         data: [12, 19, 3, 5, 2, 3],
         backgroundColor: "rgba(255, 99, 132, 0.2)",
         borderColor: "rgba(255, 99, 132, 1)",
