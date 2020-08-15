523cd967dcfcf9b710f3193f5ac29f8fca8b9588
Jan Claasen <janclaasen@arpmanet-2.local>
2020-08-16 09:01:04 +1200

    fixed importing of json file

--- static/index.js
+++ static/index.js
@@ -1,30 +1,24 @@
-// const ctx = document.getElementById("myChart").getContext("2d");
+import modelData from "./data.js";
 const canvas = document.getElementById("myChart");
 const ctx = canvas.getContext("2d");
+console.log(modelData);
 const myChart = new Chart(ctx, {
-  type: "bar",
+  type: "line",
   data: {
     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
     datasets: [
       {
         label: "# of Votes",
         data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+      },
+      {
+        label: "new thing",
+        data: [1, 20, 4, 5, 6, 7],
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
         borderWidth: 1,
       },
     ],
