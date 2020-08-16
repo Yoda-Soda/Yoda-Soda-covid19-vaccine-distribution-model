7cbbf001e6bc6897e3733d6adf19e88993a20f97
Jan Claasen <janclaasen@arpmanet-2.local>
2020-08-16 13:02:28 +1200

    added simple styles and embeded tableau dashboard

--- static/index.js
+++ static/index.js
@@ -1,58 +1,15 @@
-import modelData from "./data.js";
-const canvas = document.getElementById("myChart");
-const ctx = canvas.getContext("2d");
-
-//allows for dynamic fetching of the x-axis label
-const fetchLabel = () => {
-  let labelArray = [];
-  for (const month of modelData) {
-    if (month.state_abbrev == "ACT") {
-      labelArray.push(month.month);
-    }
-  }
-  return labelArray;
-};
-
-//dynamically fetches data per state Parameter: (state = String, property = String)
-
-const fetchData = (state, property) => {
-  let dataArray = [];
-  state = state.toUpperCase(); //converts property to uppercase in case of an accidental lowercase
-  console.log(state, property);
-
-  for (const month of modelData) {
-    if (month.state_abbrev == state) {
-      console.log(state);
-      dataArray.push(month[property]);
-    }
-  }
-  return dataArray;
-};
-console.log(fetchData("ACT", "confirmed"));
-
-const myChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: fetchLabel(),
-    datasets: [
-      {
-        label: modelData[0].state_abbrev,
-        data: fetchData("ACT", "confirmed"),
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-        borderColor: "rgba(255, 99, 132, 1)",
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
-          },
-        },
-      ],
-    },
-  },
-});
+var divElement = document.getElementById("viz1597539127115");
+var vizElement = divElement.getElementsByTagName("object")[0];
+if (divElement.offsetWidth > 800) {
+  vizElement.style.width = "1000px";
+  vizElement.style.height = "827px";
+} else if (divElement.offsetWidth > 500) {
+  vizElement.style.width = "1000px";
+  vizElement.style.height = "827px";
+} else {
+  vizElement.style.width = "100%";
+  vizElement.style.height = "1277px";
+}
+var scriptElement = document.createElement("script");
+scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
