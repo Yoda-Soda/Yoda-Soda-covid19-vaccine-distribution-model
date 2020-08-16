16e781d42e7a1d154ee62024e6aec20c0886a2a9
Jan Claasen <janclaasen@arpmanet-2.local>
2020-08-16 12:52:50 +1200

    rework point

--- static/index.js
+++ static/index.js
@@ -1,6 +1,8 @@
 import modelData from "./data.js";
 const canvas = document.getElementById("myChart");
 const ctx = canvas.getContext("2d");
+
+//allows for dynamic fetching of the x-axis label
 const fetchLabel = () => {
   let labelArray = [];
   for (const month of modelData) {
@@ -11,6 +13,23 @@ const fetchLabel = () => {
   return labelArray;
 };
 
+//dynamically fetches data per state Parameter: (state = String, property = String)
+
+const fetchData = (state, property) => {
+  let dataArray = [];
+  state = state.toUpperCase(); //converts property to uppercase in case of an accidental lowercase
+  console.log(state, property);
+
+  for (const month of modelData) {
+    if (month.state_abbrev == state) {
+      console.log(state);
+      dataArray.push(month[property]);
+    }
+  }
+  return dataArray;
+};
+console.log(fetchData("ACT", "confirmed"));
+
 const myChart = new Chart(ctx, {
   type: "line",
   data: {
@@ -18,14 +37,7 @@ const myChart = new Chart(ctx, {
     datasets: [
       {
         label: modelData[0].state_abbrev,
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-        borderColor: "rgba(255, 99, 132, 1)",
-        borderWidth: 1,
-      },
-      {
-        label: "new thing",
-        data: [1, 20, 4, 5, 6, 7],
+        data: fetchData("ACT", "confirmed"),
         backgroundColor: "rgba(255, 99, 132, 0.2)",
         borderColor: "rgba(255, 99, 132, 1)",
         borderWidth: 1,
